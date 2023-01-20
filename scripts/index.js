44bfd597ad13a6a127267816804ee9c1569e9182
Sani Rogers <sanirogers138@gmail.com>
2023-01-20 14:01:28 -0500

    made navbar and top banner

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+// geocode.xyz API (Paid per API Call)
+// open-meteo.com API (Weather by coordinates and hourly)
+// public.opendatasoft.com (American City Data)
+// puppeteer (web scraping)
+// html2pdf.js turns webpage into pdf
+// OpenTripMap API finds tourist interests
